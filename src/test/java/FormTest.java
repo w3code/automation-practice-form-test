9db7394a3fe6b31259f4e0d9fd969903d0cf92a6
Alex <qatechnix@gmail.com>
2021-10-28 17:37:55 +0700

    init: first commit

---
+++ src/test/java/FormTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class FormTest {
+
+    @Test
+    void selenideFormTest() {
+
+        // Открыть Practice Form
+        open("https://demoqa.com/automation-practice-form");
+
+    }
+}
